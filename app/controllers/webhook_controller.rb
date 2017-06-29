c28a731403ee1dbe82a441abc5c7763956eaaf41
sogawa <s.ogawa055@gmail.com>
2017-06-29 16:56:07 +0000

    modified

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -15,18 +15,16 @@ class WebhookController < ApplicationController
     
     event = params["events"][0]
     replyToken = event["replyToken"]
-    
-    print params['text']
   
     docomo_client = DocomoClient.new(api_key: ENV["DOCOMO_API_KEY"])
       response = nil
-      last_dialogue = LastDialogue.find_by(mid: event["replyToken"])
+      last_dialogue = LastDialogue.find_by(mid: params["id"])
       
       if last_dialogue.nil?
         response =  docomo_client.dialogue(params['text'])
-        last_dialogue = LastDialogue.new(mid: event["replyToken"], mode: response.body['mode'], da: response.body['da'], context: response.body['context'])
+        last_dialogue = LastDialogue.new(mid: params["id"], mode: response.body['mode'], da: response.body['da'], context: response.body['context'])
       else
-        response =  docomo_client.dialogue(params['text'], last_dialogue.mode, last_dialogue.context)
+        response =  docomo_client.dialogue(params['text'], last_dialogue.context)
         last_dialogue.mode = response.body['mode']
         last_dialogue.da = response.body['da']
         last_dialogue.context = response.body['context']
