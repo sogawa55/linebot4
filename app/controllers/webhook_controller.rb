5939e520ffaf120af57389e53682a8b9a08124d2
sogawa <s.ogawa055@gmail.com>
2017-06-30 16:33:21 +0000

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -19,7 +19,7 @@ class WebhookController < ApplicationController
   
      docomo_client = DocomoClient.new(api_key: ENV["DOCOMO_API_KEY"])
      
-     context = @redis.gets('context', params['context'])
+     context = @redis.gets('context', response.body['context'])
      
      response =  docomo_client.dialogue(user_text, context)
 
