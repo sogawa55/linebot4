545f1facdf71cca42bdfe756a5fb4132fdaa6139
sogawa <s.ogawa055@gmail.com>
2017-06-30 17:46:14 +0000

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -19,11 +19,12 @@ class WebhookController < ApplicationController
   
      docomo_client = DocomoClient.new(api_key: ENV["DOCOMO_API_KEY"])
     
-    @redis.set('user_id',params['id'])
+    redis = Redis.new
+    redis.set('user_id',params['id'])
     
-    if @redis.gets('user_id').nil? then 
+    if redis.get('user_id').nil? then 
     response =  docomo_client.dialogue(user_text, context=nil)
-    context = @redis.set('user_id',response.body['context'])
+    context = redis.set('user_id',response.body['context'])
     else
     response = docomo_client.dialogue(user_text, context)
     context = @redis.set('user_id',response.body['context'])
